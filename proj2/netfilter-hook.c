3017a9648bbf5e9d0bf27843e296454e97ee4190
kim-mins <kim-mins@naver.com>
2019-12-01 01:39:22 +0900

    hook function for pre routing done

--- proj2/netfilter-hook.c
+++ proj2/netfilter-hook.c
@@ -7,6 +7,13 @@
 #include <linux/proc_fs.h>
 #include <linux/netfilter.h>
 
+// for parsing Ipv4 address from struct sk_buff
+#define NIPQUAD(addr) \
+    ((unsigned char *)&addr)[0], \
+    ((unsigned char *)&addr)[1], \
+    ((unsigned char *)&addr)[2], \
+    ((unsigned char *)&addr)[3]
+
 // setting for proc file system
 #define PROC_DIRNAME    "proj2"
 #define PROC_FILENAME   "proj2-procfile"
@@ -18,12 +25,44 @@ static struct proc_dir_entry *proc_file;    // proc file system file
 char        port_num[PORT_NUM_SIZE];
 static int  finished = 0;
 
-/*
+
 // hook function for monitoring hook point "NF_INET_PRE_ROUTING"
-// at this point, 
 static unsigned int pre_routing_hook_impl(void *priv, struct sk_buff *skb, const struct nf_hook_state *state) {
   
   // struct sk_buff *skb is packet
+  // get ip header to get src/dest IPv4 address
+  struct iphdr  *ih = ip_hdr(skb);
+  // get tcp header to get src/dest port number
+  struct tcphdr *th = tcp_hdr(skb);
+  
+  // if destination port is 33333, then do forwarding
+  if (ntohs(th->dest) == 33333) {
+    // print captured packet info before manipulation
+    printk(KERN_INFO "Before manipulation\n")
+    printk(KERN_INFO "  PRE_ROUTING[(%u;%d;%d;%d.%d.%d.%d;%d.%d.%d.%d)]\n", 
+            ipheader->protocol,
+            ntohs(th->source),  // 33333
+            ntohs(th->dest),    // 33333
+            NIPQUAD(ih->saddr),
+            NIPQUAD(ih->daddr)
+          );
+    // manipulate src port number from 33333 to 7777
+    th->source = htons(7777);
+    // manipulate dest port number from 33333 to 7777
+    th->dest = htons(7777);
+
+    // print manipulated packet info
+    printk(KERN_INFO "After manupulation\n")
+    printk(KERN_INFO "  PRE_ROUTING[(%u;%d;%d;%d.%d.%d.%d;%d.%d.%d.%d)]\n", 
+            ipheader->protocol,
+            ntohs(th->source),  // 7777
+            ntohs(th->dest),    // 7777
+            NIPQUAD(ih->saddr),
+            NIPQUAD(ih->daddr)
+          );
+  }
+
+  return NF_ACCEPT;   // do forwarding or going up
 }
 // hook function for monitoring hook point "NF_INET_FORWARD"
 static unsigned int forward_hook_impl(void *priv, struct sk_buff *skb, const struct nf_hook_state *state) {
@@ -45,7 +84,7 @@ static struct nf_hook_ops forward_hook_struct {
   .hooknum = NF_INET_FORWARD,       // at NF_INET_FORWARD (hook point)
   .priority = NF_IP_PRI_FIRST       // set priority (doesn't matter)
 }
-*/
+
 // custom open function for open proc file
 static int my_open(struct inode *inode, struct file *file) {
 
@@ -100,10 +139,9 @@ static int __init hook_init(void) {
   // create proc file in proc file system
   proc_file = proc_create(PROC_FILENAME, 0600, proc_dir, &proc_fops);
   // register hook structures to netfilter
-  /*
   nf_register_hook(&pre_routing_hook_struct);
   nf_register_hook(&forward_hook_impl);
-  */
+  
   return 0;
 }
 
@@ -111,11 +149,12 @@ static int __init hook_init(void) {
 static void __exit hook_exit(void) {
   remove_proc_entry(PROC_FILENAME, proc_dir);
   // unregister hook structures from netfilter
-  /*
   nf_unregister_hook(&pre_routing_hook_struct);
   nf_unregister_hook(&forward_hook_impl);
-  */
+  
   printk(KERN_INFO "LKM ends..\n");
+
+  return;
 }
 
 module_init(hook_init);
